f47293095f90bf247c6aa7bc869811238f8842e6
Sarah Ronald <sareronald@hotmail.com>
2020-09-26 14:27:07 +1000

    local storage is working

--- script.js
+++ script.js
@@ -27,76 +27,79 @@ function addCityToList(event) {
 searchBtn.addEventListener("click", addCityToList);
 
 var storedCities = JSON.parse(localStorage.getItem("city-names"));
+
 // if storedCities is an array and its length in greater than 0 override cities
-if (storedCities.length > 0)
-  //console.log("the array is empty");
+if (storedCities.length > 0) {
+  cities = storedCities;
+  // append the last storedCities on to page
+  citiesContainer.append(storedCities.slice(-1)[0]);
+  // how do I get these to display as cards - like when I search them?
+}
+console.log(cities);
 
-  function getWeather() {
-    var queryURL =
-      "https://api.openweathermap.org/data/2.5/weather?" +
-      "q=" +
-      currentCity +
-      "&appid=" +
-      APIKey;
-
-    //AJAX call to the OpenWeatherMap API
-    $.ajax({
-      url: queryURL,
-      method: "GET",
-    })
-
-      //Here we store all of the retreived data inside of an object called "response"
-      .then(function (response) {
-        console.log(queryURL);
-        console.log(response);
-        var date = document.getElementById("todays-date");
-        date.textContent = moment().format("DD MM YYYY");
-        //Transfer content to HTML
-        $("#city").html("<h3>" + response.name + "</h3>");
-        $("#todays-icon").html(
-          "<img src='https://openweathermap.org/img/wn/" +
-            response.weather[0].icon +
-            "@2x.png'>"
-        );
-        $("#humidity").text("Humidity: " + response.main.humidity + "%");
-        $("#wind").text("Wind Speed: " + response.wind.speed + " MPH");
-        // add UV index here
-        // $(".uv-index"). text("UV Index: " + response.)
-        // convert the kelvin to celcius
-        var tempT = Math.round(response.main.temp - 273.15);
-        // add temp content to html
-        $("#temp").html("Temperature: " + tempT + "&degC");
-
-        $();
-      });
-  }
-
-function getUV() {
-  var latitude = response.coord.lat;
-  var longitude = response.coord.lat;
-  console.log(latitude);
-  var gueryURLuv =
-    "http://api.openweathermap.org/data/2.5/uvi?" +
-    "lat=" +
-    latitude +
-    "&lon=" +
-    longitude +
+function getWeather() {
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?" +
+    "q=" +
+    currentCity +
     "&appid=" +
     APIKey;
 
-  //AJAX call to the OpenWeatherMap API - UV index
+  //AJAX call to the OpenWeatherMap API
   $.ajax({
-    url: queryURLuv,
+    url: queryURL,
     method: "GET",
   })
 
-    // Here we store all of the retreive data inside of an object called "response"
+    //Here we store all of the retreived data inside of an object called "response"
     .then(function (response) {
-      cpmsp;
-      event.log(queryURLuv);
+      console.log(queryURL);
       console.log(response);
-      // Transfer content to HTML
-      $("#uv-index").text(response.value);
+      var date = document.getElementById("todays-date");
+      date.textContent = moment().format("DD / MM / YYYY");
+      //Transfer content to HTML
+      $("#city").html("<h3>" + response.name + "</h3>");
+      $("#todays-icon").html(
+        "<img src='https://openweathermap.org/img/wn/" +
+          response.weather[0].icon +
+          "@2x.png'>"
+      );
+      $("#humidity").text("Humidity: " + response.main.humidity + "%");
+      $("#wind").text("Wind Speed: " + response.wind.speed + " MPH");
+      // convert the kelvin to celcius
+      var tempT = Math.round(response.main.temp - 273.15);
+      // add temp content to html
+      $("#temp").html("Temperature: " + tempT + "&degC");
+      // add UV index here
+      // $(".uv-index"). text("UV Index: " + response.)
+      var latitude = response.coord.lat;
+      var longitude = response.coord.lon;
+      console.log(latitude);
+      console.log(longitude);
+      var queryURLuv =
+        "http://api.openweathermap.org/data/2.5/uvi?" +
+        "lat=" +
+        latitude +
+        "&lon=" +
+        longitude +
+        "&appid=" +
+        APIKey;
+
+      //AJAX call to the OpenWeatherMap API - UV index
+      $.ajax({
+        url: queryURLuv,
+        method: "GET",
+      })
+
+        // Here we store all of the retreive data inside of an object called "response"
+        .then(function (response) {
+          console.log(queryURLuv);
+          console.log(response);
+          // Transfer content to HTML
+          $("#uv-index").text("UV Index: " + response.value);
+        });
+
+      $();
     });
 }
 
@@ -119,18 +122,24 @@ function getForecast() {
       console.log(queryURLf);
       console.log(response);
       // Transfer content to HTML
-      $(".forecast").html("<h3>" + "5-Day Forecast: " + "</h3>");
-      $(".forecast-date").text(response.list[0].dt_txt);
-      var tempF = Math.round(response.list[0].main.temp - 273.15);
-      $(".forecast-temp").text("Temp:" + tempF + "&degC");
-      $(".forecast-humidity").text(
-        "Humidity:" + response.list[0].main.humidity + "%"
-      );
-
-      for (var i = 0; i <= 4; i++) {
+      var forecastEl = document.querySelectorAll(".new-forecast");
+      for (i = 0; i < forecastEl.length; i++) {
         var newCity = $("<div>");
         newCity.attr("data-name", city[i]);
         newCity.text(city[i]);
+        $(".forecast").html("<h3>" + "5-Day Forecast: " + "</h3>");
+        $("#forecast-date1").text(response.list[0].dt_txt);
+        $("#forecast-symbol1").html(
+          "<img src='https://openweathermap.org/img/wn/" +
+            response.list[0].weather.icon +
+            "@2x.png)>'"
+        );
+        var tempF = Math.round(response.list[0].main.temp - 273.15);
+        $("#forecast-temp1").text("Temp:" + tempF + "&degC");
+        $("#forecast-humidity1").text(
+          "Humidity:" + response.list[0].main.humidity + "%"
+        );
+        // I initially had the for statement here: this is the rest of what I had
 
         $();
       }
